6dba03b2f7e1e9d90c97e593c681c2c08673066d
carloscaro <carlosjesuscaro@icloud.com>
2021-01-24 18:38:40 -0500

    explotting through plotting

--- main.R
+++ main.R
@@ -1,1 +1,19 @@
 # Banking recovery regression
+
+# Importing libraries
+library(dplyr)
+library(car)
+
+data <- read.csv('bank_data.csv', header=TRUE)
+summary(data)
+glimpse(data)
+
+table(data$recovery_strategy)
+attach(data)
+plot(age, expected_recovery_amount)
+plot(age, actual_recovery_amount)
+boxplot(expected_recovery_amount, sex)
+
+
+
+
